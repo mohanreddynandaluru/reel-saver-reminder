2b1ebc88fcc4f7a6a0bf37a9621ea3182f80790d
mohanreddynandaluru <mohanreddy232312@gmail.com>
2025-07-11 01:38:12 +0530

    backend for reel-saver

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/insta-not
 mongoose.connect(mongoUri)
   .then(() => {
   // ✅ Connected to MongoDB
+  console.log("Connected to MongoDB");
 })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
